dcec9dc1298ed5084941bbb580eb345e8cdbbbae
garg-anuj <anujgarg2018@gmail.com>
2025-05-30 00:27:35 +0530

    apiFetcher Hook

---
+++ src/hooks/useApiFetcher.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useApiFetcher = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setError(null);
+
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return [data, isLoading, error];
+};
+
+export default useApiFetcher;
